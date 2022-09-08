f6e9d3fee747fe3178b67e7d547536379a358c5f
Scott Clayton <sclayton@bamboohr.com>
2022-09-08 13:55:06 -0600

    ZZ-967
    1) Add meganav to /resources/guides pages.
    2) Handle new meganav header height.
    3) Refactored quote block to handle source links.
    4) Some styling for compensation strategy page--more to come.

--- blog/blocks/image/image.js
+++ blog/blocks/image/image.js
@@ -11,7 +11,12 @@ function buildColumns(rowEl, count) {
 }
 
 export default function decorateImage(blockEl) {
+  if (blockEl.classList.contains('med-width')) blockEl.parentElement.classList.add('med-width');
+  if (blockEl.classList.contains('full-width')) blockEl.parentElement.classList.add('full-width');
   if (blockEl.classList.contains('extra-wide')) blockEl.parentElement.classList.add('extra-wide');
+  if ([...blockEl.classList].some((c) => c.startsWith('bg-'))) {
+    blockEl.parentElement.classList.add('background-image');
+  }
   const blockCount = blockEl.firstChild.childElementCount;
   if (blockCount > 1) {
     buildColumns(blockEl.firstChild, blockCount);
