3ff496558014fa246855c84f4d3115c2858ca587
Scott Clayton <sclayton@bamboohr.com>
2022-09-13 17:55:41 -0600

    ZZ-1028
    1) Use '/nav' as default if not blog.
    2) Use existing footer but if not blog hide the blog section.

--- blog/blocks/footer/footer.js
+++ blog/blocks/footer/footer.js
@@ -6,13 +6,17 @@ import { decorateIcons } from '../../scripts/scripts.js';
  */
 
 export default async function decorate(block) {
+  const hideBlog = window.location.pathname.startsWith('/blog/') ? false : true;
   const resp = await fetch('/blog/fixtures/footer.plain.html');
   const html = await resp.text();
   block.innerHTML = html;
   decorateIcons(block);
   const styles = ['company', 'support', 'links', 'blog', 'social', 'brand', 'legal'];
   styles.forEach((style, i) => {
-    if (block.children[i]) block.children[i].classList.add(style);
+    if (block.children[i]) {
+      block.children[i].classList.add(style);
+      if (hideBlog && style === 'blog') block.children[i].setAttribute('aria-hidden', true);
+    }
   });
 
   const $teconsent = document.createElement('div');
