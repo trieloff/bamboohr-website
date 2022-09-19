772284dd1c4b511b14ba7b2912f1f90ec6bc21fd
Jack Wetmore <jack.wetmore@hoodoo.digital>
2022-09-19 15:24:09 -0600

    Finishing up Multi Element Adding JS to manipulate the image locations depending on screen size Adding in gray shade5/10 box-shadows

--- blog/blocks/multi-element/multi-element.js
+++ blog/blocks/multi-element/multi-element.js
@@ -1,5 +1,25 @@
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+function moveImages(block, matched) {
+  const images = block.querySelectorAll('.has-images');
+  const p = block.querySelector('p:first-of-type');
+
+  images.forEach((image) => {
+    if (matched) {
+      // move images into content section
+      p.before(image);
+    } else {
+      // put 'em back
+      block.querySelector(':scope > div').append(image);
+    }
+  });
+}
+
 export default function decorate(block) {
   const cols = block.querySelectorAll(':scope > div > div');
+
+  if (block.classList.contains('full-width')) block.parentElement.classList.add('full-width');
+  if (block.classList.contains('mid-width')) block.parentElement.classList.add('mid-width');
 
   // convert "number" classes
   [...block.classList].forEach((name) => {
@@ -22,4 +42,14 @@ export default function decorate(block) {
       });
     }
   });
+
+  // hero duplication
+  if (block.classList.contains('hero') || !block.classList.contains('logo-cards')) {
+    console.log(block);
+    moveImages(block, mediaQuery.matches);
+
+    mediaQuery.addEventListener('change', (event) => {
+      moveImages(block, event.matches);
+    });
+  }
 }
