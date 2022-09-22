b08d26994c6b01a40f7acd2c4467175f12800f30
Meng Tian <mtian@bamboohr.com>
2022-09-22 09:28:48 -0600

    mt add chilipiper back to test

--- blog/blocks/form/form.js
+++ blog/blocks/form/form.js
@@ -440,18 +440,18 @@ function loadFormAndChilipiper(formId, successUrl, chilipiper) {
     });
   });
   if (chilipiper) {
-    // loadScript('https://js.chilipiper.com/marketing.js', () => {
-    // eslint-disable-next-line
-    //   window.q = (a) => {return function(){ChiliPiper[a].q=(ChiliPiper[a].q||[]).concat([arguments])}};
-    // eslint-disable-next-line
-    //   window.ChiliPiper=window.ChiliPiper||"submit scheduling showCalendar submit widget bookMeeting".split(" ").reduce(function(a,b){a[b]=q(b);return a},{});
-    //   // eslint-disable-next-line
-    //   ChiliPiper.scheduling('bamboohr', `'${chilipiper}'`, {
-    //     title: 'Thanks! What time works best for a quick call?',
-    //     onRouted: setTimeout(() => { window.location.href = successUrl; }, '240000'),
-    //     map: true,
-    //   });
-    // });
+    loadScript('https://js.chilipiper.com/marketing.js', () => {
+      // eslint-disable-next-line
+      window.q = (a) => {return function(){ChiliPiper[a].q=(ChiliPiper[a].q||[]).concat([arguments])}};
+      // eslint-disable-next-line
+      window.ChiliPiper=window.ChiliPiper||"submit scheduling showCalendar submit widget bookMeeting".split(" ").reduce(function(a,b){a[b]=q(b);return a},{});
+      // eslint-disable-next-line
+      ChiliPiper.scheduling('bamboohr', `'${chilipiper}'`, {
+        title: 'Thanks! What time works best for a quick call?',
+        onRouted: setTimeout(() => { window.location.href = successUrl; }, '240000'),
+        map: true,
+      });
+    });
   }
 }
 
