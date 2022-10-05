ebc0cbfbea1c658f18b346630ca0e64bc0b300de
Scott Clayton <sclayton@bamboohr.com>
2022-10-05 11:51:23 -0600

    ZZ-1094
    Fixing based on stakeholder feedback.

--- blog/blocks/title/title.js
+++ blog/blocks/title/title.js
@@ -1,3 +1,4 @@
 export default function decorate(block) {
   if (block.classList.contains('normal-width')) block.parentElement.classList.add('normal-width');
+  if (block.classList.contains('extra-small-width')) block.parentElement.classList.add('extra-small-width');
 }
