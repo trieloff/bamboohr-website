4b1cc928086d7936ce6501d32a950b905e35c99e
Greg Roberts <gregorysroberts@gmail.com>
2022-10-10 09:52:15 -0600

    Adds quote styles
    Adds new quote styles for company name and secondary colors for company name and title.

--- blog/blocks/quote/quote.js
+++ blog/blocks/quote/quote.js
@@ -1,12 +1,20 @@
 export default function decorate(block) {
   const byline = block.querySelector('em');
+  const company = block.querySelector('h4');
   if (block.classList.contains('full-width')) block.parentElement.classList.add('full-width');
   if (block.classList.contains('normal-width')) block.parentElement.classList.add('normal-width');
 
   // Adding image class to block children
   [...block.children].forEach((row) => {
-    [...row.children].forEach((cell) => cell.classList.add(cell.querySelector('img') ? 'image' : 'content'));
+    [...row.children].forEach((cell) =>
+      cell.classList.add(cell.querySelector('img') ? 'image' : 'content')
+    );
   });
+
+  // if company
+  if (company) {
+    company.classList.add('company');
+  }
 
   // if byline
   if (byline) {
