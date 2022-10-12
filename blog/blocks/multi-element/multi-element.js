344211ce33838035bfa28e1c735e1ce3be6d350c
Jack Wetmore <jack.wetmore@hoodoo.digital>
2022-10-12 12:24:22 -0600

    Prettier update

--- blog/blocks/multi-element/multi-element.js
+++ blog/blocks/multi-element/multi-element.js
@@ -1,19 +1,19 @@
 export default function decorate(block) {
   // convert "number" classes
   [...block.classList].forEach((name) => {
-    if (!Number.isNaN(+name.split("").at(0))) {
+    if (!Number.isNaN(+name.split('').at(0))) {
       block.classList.remove(name);
       block.classList.add(`style-${name}`);
     }
   });
 
   // clean up markup, move all "columns" as block children
-  block.querySelectorAll(":scope > div").forEach((row, key) => {
-    row.querySelectorAll(":scope > div").forEach((col) => {
-      const pictures = col.querySelectorAll("picture");
+  block.querySelectorAll(':scope > div').forEach((row, key) => {
+    row.querySelectorAll(':scope > div').forEach((col) => {
+      const pictures = col.querySelectorAll('picture');
 
       if (pictures.length > 1) {
-        col.classList.add("images");
+        col.classList.add('images');
 
         // move to parent div and remove empty p
         pictures.forEach((picture) => {
@@ -21,15 +21,14 @@ export default function decorate(block) {
           col.append(picture);
         });
       } else {
-        col.classList.add("content");
+        col.classList.add('content');
       }
 
       if (
         key === 1 &&
-        (block.classList.contains("extra") ||
-          block.classList.contains("extra-right"))
+        (block.classList.contains('extra') || block.classList.contains('extra-right'))
       ) {
-        col.classList.add(`extra`);
+        col.classList.add('extra');
       }
 
       block.append(col);
