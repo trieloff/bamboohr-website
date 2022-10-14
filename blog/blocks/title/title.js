5570e1ad9be1dd0b7b13ed2e11de7c7750330641
Scott Clayton <sclayton@bamboohr.com>
2022-10-14 10:39:56 -0600

    Backgrounds and icons
    1) Limit height on top and bottom backgrounds.
    2) Center align dual tone hex icons

--- blog/blocks/title/title.js
+++ blog/blocks/title/title.js
@@ -1,4 +1,5 @@
 export default function decorate(block) {
   if (block.classList.contains('normal-width')) block.parentElement.classList.add('normal-width');
+  if (block.classList.contains('med-width')) block.parentElement.classList.add('med-width');
   if (block.classList.contains('extra-small-width')) block.parentElement.classList.add('extra-small-width');
 }
