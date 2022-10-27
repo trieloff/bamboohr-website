85f78eb3dc3b4ef61b3341206c2666dbd9518908
Jack Wetmore <jack.wetmore@hoodoo.digital>
2022-10-27 14:11:57 -0600

    Move cards portion of JS to function Return empty when nothing so "null" doesn't get appended anywhere Finish responsive styles for non-cards area

--- blog/blocks/hero/hero.js
+++ blog/blocks/hero/hero.js
@@ -5,6 +5,9 @@ function setupImages(block) {
   const imageGroupContainer = createElem('div', 'image-group-container');
   const imageParents = block.querySelectorAll(':scope > div:nth-child(n + 2)');
 
+  // skip if none
+  if (!block.querySelector('picture')) return '';
+
   imageParents.forEach((imageParent) => {
     const imageContainer = createElem('div', 'image-container');
     const caption = imageParent.querySelector(':scope > div:nth-child(2) p:nth-child(2)');
@@ -28,6 +31,9 @@ function setupContent(block) {
   const content = block.querySelectorAll(':scope > div:first-child > div:first-child')?.item(0);
   const buttons = content.querySelectorAll(':scope .button-container');
 
+  // skip if none
+  if (!content) return '';
+
   if (buttons.length > 0) {
     const ctaContainer = createElem('div', 'cta-container');
 
@@ -38,37 +44,36 @@ function setupContent(block) {
   return content;
 }
 
+function setupCards(block) {
+  // get cards and decorate
+  const cards = createElem('div', 'cards');
+  cards.classList.add('block', '2-columns');
+  block.querySelectorAll(':scope > :not(:first-child)').forEach((cardRow) => cards.append(cardRow));
+
+  // skip if none
+  if (!cards) return '';
+
+  // use card block decorate
+  decorateCards(cards);
+
+  // load css
+  const cssBase = `${window.hlx.serverPath}${window.hlx.codeBasePath}`;
+  loadCSS(`${cssBase}/blocks/cards/cards.css`, null);
+
+  return cards;
+}
+
 export default function decorate(block) {
-  if (block.classList.contains('cards-2-column')) {
+  if (block.classList.contains('triple-hex')) {
+    block.closest('.hero-container').classList.add('triple-hex');
+  } else if (block.classList.contains('cards-2-column')) {
     block.closest('.hero-container').classList.add('cards-2-column');
-    // get content
-    const content = block.querySelector(':scope > div:first-child > div:first-child');
-    content.classList.add('content');
-
-    // get cards and decorate
-    const cards = createElem('div', 'cards');
-    cards.classList.add('block', '2-columns');
-    block
-      .querySelectorAll(':scope > :not(:first-child)')
-      .forEach((cardRow) => cards.append(cardRow));
-    decorateCards(cards);
-
-    // load css
-    const cssBase = `${window.hlx.serverPath}${window.hlx.codeBasePath}`;
-    loadCSS(`${cssBase}/blocks/cards/cards.css`, null);
-
-    // empty block include content and cards
-    block.innerText = '';
-    block.append(content, cards);
-  } else {
-    if (block.classList.contains('triple-hex')) {
-      block.closest('.hero-container').classList.add('triple-hex');
-    }
-
-    const content = setupContent(block);
-    const imagesContainer = setupImages(block);
-
-    block.innerHTML = '';
-    block.append(content, imagesContainer);
   }
+
+  const content = setupContent(block);
+  const imagesContainer = setupImages(block);
+  const cards = setupCards(block);
+
+  block.innerHTML = '';
+  block.append(content, imagesContainer, cards);
 }
