0e9488f651ea8eee2647d974d7db958b68fb6de4
Jack Wetmore <jack.wetmore@hoodoo.digital>
2022-10-28 15:32:31 -0600

    Starting Benefits block

---
+++ blog/blocks/benefits/benefits.js
@@ -0,0 +1,42 @@
+import { createElem, decorateBackgrounds } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const title = block.querySelector(':scope > div:first-child > div:first-child');
+  title.classList.add('title');
+  title.classList.add('bg-block-benefits');
+  decorateBackgrounds(title);
+
+  const benefits = block.querySelectorAll(':scope > div:not(:first-child)');
+  const col1 = createElem('div', 'benefits-col-1');
+  const col2 = createElem('div', 'benefits-col-2');
+
+  block.innerText = '';
+  block.append(title);
+
+  benefits.forEach((benefit, key) => {
+    // move icon to parent div, remove empty p
+    const icon = benefit.querySelector('span.icon');
+    icon.parentNode.remove();
+    benefit.append(icon);
+
+    // move title to parent div
+    const benefitTitle = benefit.querySelector('h3');
+    benefit.append(benefitTitle);
+
+    // move text to parent div, remove empty div
+    const text = benefit.querySelector('p:not(:empty)');
+    text.parentNode.remove();
+    benefit.append(text);
+
+    benefit.classList.add('benefit');
+
+    if (key < 4) {
+      col1.append(benefit);
+    } else {
+      col2.append(benefit);
+    }
+  });
+
+  block.append(col1);
+  if (col2.children.length > 0) block.append(col2);
+}
