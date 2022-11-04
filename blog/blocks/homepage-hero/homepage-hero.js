e34ff79845af3990d6c288bf6ed7081436bffdad
Remington Reed <RemReed@rightpoint.com>
2022-11-04 13:46:38 -0600

    ZZ-1251 - WIP - Initial work on homepage hero. The styles on the top portion of the hero still may need a bit of work, the carousel hasnt really been touched yet.

---
+++ blog/blocks/homepage-hero/homepage-hero.js
@@ -0,0 +1,127 @@
+import { createElem } from '../../scripts/scripts.js';
+
+const MIN_BREAKPOINTS = {
+  'tablet': '600px',
+  'laptop': '1025px',
+  'desktop': '1800px',
+}
+
+const IMAGE_WIDTHS = {
+  'tablet': '1024',
+  'laptop': '1440',
+  'desktop': '2000',
+}
+
+const IMAGE_OPTIMIZATION = 'medium';
+
+// TODO: would probably be best to make this a global utility
+function buildPicture(images) {
+  const pictureElem = createElem('picture', 'homepage-hero-background');
+
+  Object.keys(images).forEach((key) => {
+    const sourceWebpElem = createElem('source');
+
+    const srcsetPath = `${images[key]}?width=${IMAGE_WIDTHS[key]}&optimize=${IMAGE_OPTIMIZATION}`;
+
+    // Since tablet is the smallest breakpoint in our case, we'll use it as default
+    if (key === 'tablet') {
+      const imgPngElem = createElem('img');
+      imgPngElem.setAttribute('loading', 'lazy');
+      imgPngElem.setAttribute('type', 'image/png');
+      imgPngElem.setAttribute('alt', '');
+      imgPngElem.setAttribute('srcset', `${srcsetPath}&format=png`);
+
+      pictureElem.append(imgPngElem);
+    } else {
+      const sourcePngElem = createElem('source');
+
+      sourceWebpElem.setAttribute('media', `(min-width: ${MIN_BREAKPOINTS[key]})`);
+      sourcePngElem.setAttribute('media', `(min-width: ${MIN_BREAKPOINTS[key]})`);
+
+      sourcePngElem.setAttribute('type', 'image/png');
+      sourcePngElem.setAttribute('srcset', `${srcsetPath}&format=png`);
+
+      pictureElem.prepend(sourcePngElem);
+    }
+
+    sourceWebpElem.setAttribute('type', 'image/webp');
+    sourceWebpElem.setAttribute('srcset', `${srcsetPath}&format=webp`);
+    pictureElem.prepend(sourceWebpElem);
+  });
+
+  console.log('PICTURE', pictureElem);
+
+  return pictureElem;
+}
+
+function processBackground(block) {
+  const parts = block.querySelectorAll(':scope > div');
+
+  const images = {};
+  parts.forEach((part) => {
+    const partChildren = part.children;
+    if (partChildren.length > 1) {
+      const key = partChildren.item(0).innerText.toLowerCase();
+
+      if (key.endsWith('image')) {
+        const imageType = key.substring(0, key.indexOf(' '));
+        const imagePathFull = partChildren.item(1).firstElementChild.firstElementChild.srcset;
+        const imagePath = imagePathFull.substr(0, imagePathFull.indexOf('?'));
+
+        images[imageType] = imagePath;
+
+        part.remove();
+      }
+    }
+  });
+
+  if (Object.keys(images).length > 0) {
+    const backgroundPictureElem = buildPicture(images);
+
+    const homepageHeroSection = block.closest('.homepage-hero-container');
+
+    homepageHeroSection.prepend(backgroundPictureElem);
+  }
+}
+
+function processCarousel(block) {
+  const parts = block.querySelectorAll(':scope > div');
+
+  const carouselContainer = createElem('div', 'homepage-hero-carousel');
+
+  parts.forEach((part) => {
+    const partChildren = part.children;
+
+    if (partChildren.length > 1) {
+      const key = partChildren.item(0).innerText.toLowerCase();
+
+      if (key === 'carousel') {
+        carouselContainer.append(partChildren.item(1));
+      }
+
+      part.remove();
+    }
+  });
+
+  block.append(carouselContainer);
+}
+
+export default function decorate(block) {
+  processBackground(block);
+  processCarousel(block);
+
+  const mainContentContainer = block.firstElementChild;
+  mainContentContainer.classList.add('homepage-hero-main-container');
+  const mainContent = mainContentContainer.firstElementChild;
+  mainContent.classList.add('homepage-hero-main-content');
+  const heroCtas = mainContent.querySelectorAll('.button-container');
+  if (heroCtas.length) {
+    const heroCtasContainer = createElem('div', 'homepage-hero-buttons');
+
+    heroCtas.forEach((buttonContainer) => {
+      heroCtasContainer.append(buttonContainer);
+    });
+
+    mainContent.append(heroCtasContainer);
+  }
+}
