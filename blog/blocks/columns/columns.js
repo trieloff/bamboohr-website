a92c69df9bb6f969883d13453d5b9d774b2001c5
Scott Clayton <sclayton@bamboohr.com>
2022-11-14 12:06:18 -0700

    ZZ-1252
    1) Multi-element columns with Wistia
    2) New icons needed for homepage.

--- blog/blocks/columns/columns.js
+++ blog/blocks/columns/columns.js
@@ -1,4 +1,80 @@
-import { hasClassStartsWith, getValuesFromClassName } from '../../scripts/scripts.js';
+import { hasClassStartsWith, getValuesFromClassName, loadCSS } from '../../scripts/scripts.js';
+import decorateWistia from '../wistia/wistia.js';
+
+function addLinkToIconSVG(icon, link) {
+  // Clone the link: 
+  if (link?.tagName === 'A') {
+    const imageLink = link.cloneNode(true);
+    imageLink.innerText = '';
+    imageLink.classList.add('column-svg-link');
+    imageLink.append(icon.firstElementChild);
+    icon.append(imageLink);
+  } 
+}
+
+function addIconContainer(col) {
+  if (!col.classList.contains('columns-title-span')) {
+    const icons = col.querySelectorAll('span.icon');
+    if (icons.length) {
+      const iconContainer = document.createElement('div');
+      iconContainer.classList.add('column-small-icons-container', 'column-multi-element');
+
+      icons.forEach(icon => {
+        let link = icon.parentElement.querySelector('a');
+        if (link) {
+          const buttonContainer = document.createElement('p');
+          buttonContainer.classList.add('button-container');
+          buttonContainer.append(link);
+          icon.parentElement.append(buttonContainer);
+
+          addLinkToIconSVG(icon, link);
+        } else if (icon.parentElement.nextElementSibling?.tagName === 'P'
+            && icon.parentElement.nextElementSibling.classList.contains('button-container')) {
+          link = icon.parentElement.nextElementSibling.firstElementChild;
+          if (link?.tagName === 'A') link.classList.remove('button', 'accent', 'small');
+          addLinkToIconSVG(icon, link);
+          icon.parentElement.append(icon.parentElement.nextElementSibling);
+        }
+
+        if (link) iconContainer.append(icon.parentElement);
+      });
+
+      if (iconContainer.children) col.appendChild(iconContainer);
+    }
+  }
+}
+
+function addWistia(col, loadWistiaCSS) {
+  const wistiaBlock = document.createElement('div');
+  wistiaBlock.classList.add('wistia', 'block');
+
+  const colChildren = [...col.children];
+  let addAllChildren = false;
+  if (colChildren.length == 2 &&
+      colChildren[0].firstElementChild.tagName === 'PICTURE' &&
+      colChildren[1].firstElementChild.tagName === 'A') {
+    addAllChildren = true;
+  }
+
+  colChildren?.forEach((child) => {
+    if (addAllChildren || child.querySelector('a')?.href?.includes('wistia')) {
+      if (!addAllChildren) col.insertBefore(wistiaBlock, child);
+      wistiaBlock.append(child);
+    }
+  });
+
+  if (addAllChildren) col.append(wistiaBlock);
+  decorateWistia(wistiaBlock);
+
+  if (loadWistiaCSS) {
+    // load css
+    const cssBase = `${window.hlx.serverPath}${window.hlx.codeBasePath}`;
+    loadCSS(`${cssBase}/blocks/wistia/wistia.css`, null);
+    loadWistiaCSS = false;
+  }
+
+  col.classList.add('img-col');
+}
 
 function buildSplit(splitVal) {
   const newCol = document.createElement('div');
@@ -17,6 +93,63 @@ function findSplitSubType(val) {
   }
 
   return isSplitSubType ? splitVals : null;
+}
+
+function setupColumns(cols, splitVals, block, needToLoadWistiaCSS) {
+  const extraSplits = splitVals.length > 2 ? 1 : 0;
+  const colParent = cols[0].parentElement;
+  let loadWistiaCSS = needToLoadWistiaCSS;
+  const colsToRemove = [];
+
+  colParent.classList.add('column-flex-container');
+  let hasImage = false;
+  let hasWistia = false;
+  cols.forEach((col, i) => {
+    col.classList.add(`column${splitVals[i + extraSplits]}`);
+
+    if (col.innerText.toLowerCase() === 'title span') {
+      if (colParent.nextElementSibling) {
+        const secondRowCols = [...colParent.nextElementSibling.children];
+        setupColumns(secondRowCols, splitVals, block, loadWistiaCSS);
+      }
+
+      cols[1].classList.add('columns-title-span');
+      colsToRemove.push(col);
+    } else if (col.querySelector('a')?.href?.includes('wistia')) {
+      addWistia(col, loadWistiaCSS);
+      hasImage = true;
+      hasWistia = true;
+    } else if (col.querySelector('img')) {
+      col.classList.add('img-col');
+      hasImage = true;
+    } else col.classList.add('non-img-col');
+    const isButtonLinks = block.classList.contains('button-style-link');
+    const buttons = col.querySelectorAll('a.button');
+
+    if (isButtonLinks) {
+      buttons.forEach((button) => {
+        button.classList.add('link');
+        button.parentElement.classList.add('left');
+      });
+    } else {
+      buttons.forEach((button) => {
+        button.classList.add('small');
+        button.parentElement.classList.add('left');
+      });
+    }
+
+    addIconContainer(col);
+  });
+
+  colsToRemove.forEach((col) => col.remove() );
+
+  if (hasWistia || !hasImage) colParent.classList.add('columns-align-start');
+
+  if (extraSplits) {
+    // Add extra column splits for cases like: 2/5/3/2 or 4/8/4
+    if (splitVals[0] !== '0') colParent.insertBefore(buildSplit(splitVals[0]), cols[0]);
+    if (splitVals[3] && splitVals[3] !== '0') colParent.appendChild(buildSplit(splitVals[3]));
+  }
 }
 
 export default function decorate(block) {
@@ -59,39 +192,7 @@ export default function decorate(block) {
     });
 
     if (splitVals) {
-      const extraSplits = splitVals.length > 2 ? 1 : 0;
-      const colParent = cols[0].parentElement;
-
-      colParent.classList.add('column-flex-container');
-      let hasImage = false;
-      cols.forEach((col, i) => {
-        col.classList.add(`column${splitVals[i + extraSplits]}`);
-        if (col.querySelector('img')) {
-          col.classList.add('img-col');
-          hasImage = true;
-        } else col.classList.add('non-img-col');
-        const isButtonLinks = block.classList.contains('button-style-link');
-        const buttons = col.querySelectorAll('a.button');
-
-        if (isButtonLinks) {
-          buttons.forEach((button) => {
-            button.classList.add('link');
-            button.parentElement.classList.add('left');
-          });
-        } else {
-          buttons.forEach((button) => {
-            button.classList.add('small');
-            button.parentElement.classList.add('left');
-          });
-        }
-      });
-      if (!hasImage) colParent.classList.add('columns-align-start');
-
-      if (extraSplits) {
-        // Add extra column splits for cases like: 2/5/3/2 or 4/8/4
-        if (splitVals[0] !== '0') colParent.insertBefore(buildSplit(splitVals[0]), cols[0]);
-        if (splitVals[3] && splitVals[3] !== '0') colParent.appendChild(buildSplit(splitVals[3]));
-      }
+      setupColumns(cols, splitVals, block, true);
     }
   } else if (cols.length === 1) {
     if (block.classList.contains('button-style-link')) {
