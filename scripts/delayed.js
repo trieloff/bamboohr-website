acf6c5fc35f49f4aa3f538f59a459e41f7e6b940
Scott Clayton <sclayton@bamboohr.com>
2023-01-30 12:11:00 -0700

    ZZ-1511
    Load google optimize in delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -130,6 +130,8 @@ loadScript('header', adobeTagsSrc, async () => {
   });
 });
 
+loadScript('header', 'https://www.googleoptimize.com/optimize.js?id=OPT-PXL7MPD', null, 'text/javascript');
+
 /* google tag manager */
 // eslint-disable-next-line
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-ZLCX');
