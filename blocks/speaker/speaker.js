3e3add739af7e66097f7269ecd9d755c6adef6ac
Meng Tian <mtian@bamboohr.com>
2023-02-03 16:16:50 -0700

    mt add speaker block

---
+++ blocks/speaker/speaker.js
@@ -0,0 +1,35 @@
+import {
+  createOptimizedPicture,
+  getMetadata,
+  toClassName,
+  loadFragment,
+} from '../../scripts/scripts.js';
+
+export default async function decorate(block) {
+  let speakers = getMetadata('speaker');
+  if (speakers) {
+    speakers = speakers.split(',').map((speaker) => toClassName(speaker.trim()));
+  }
+  console.log(speakers);
+  speakers.forEach(async (speaker) => {
+    const fragment = await loadFragment(`/fragments/speaker/${speaker}`);
+    console.log(fragment);
+  });
+  // const authorPath = `/blog/author/${toClassName(getMetadata('speaker'))}`;
+  // const resp = await fetch(`${authorPath}.plain.html`);
+  // const text = await resp.text();
+  // const dom = new DOMParser().parseFromString(text, 'text/html');
+  // const img = dom.querySelector('img');
+  // img.closest('p').remove();
+  // const fullname = dom.querySelector('h1');
+  // fullname.remove();
+  // const title = dom.querySelector('p');
+  // if (title) title.remove();
+
+  // const picture = createOptimizedPicture(img.src, false, [{ width: '200' }]);
+  // block.innerHTML = `<div class="author-image">${picture.outerHTML}</div>
+  // <div class="author-description">
+  //   <a href="${authorPath}">${fullname.textContent}</a>
+  //   ${title ? title.outerHTML : ''}
+  // </div>`;
+}
