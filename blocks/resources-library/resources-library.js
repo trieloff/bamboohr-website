0d5ef3f2792c0e4725ec099de969531f95e699d5
Greg Roberts <gregorysroberts@gmail.com>
2023-02-06 16:17:41 -0700

    Update resources-library.js

--- blocks/resources-library/resources-library.js
+++ blocks/resources-library/resources-library.js
@@ -7,6 +7,9 @@ import {
 import { sortOptions } from '../app-cards/app-cards.js';
 
 export function createResourceCard(app, prefix) {
+  if (app.image == '') {
+    return '';
+  } else {
     const card = document.createElement('div');
     card.className = `${prefix}-card`;
     const cardLinkText = app.formSubmitText ? app.formSubmitText : 'Free Download';
@@ -14,64 +17,63 @@ export function createResourceCard(app, prefix) {
 
     card.innerHTML = `<a href="${app.path}"><div class="${prefix}-card-image">${picture}</div></a><div class="${prefix}-card-copy-container"><p>${app.description}</p><a href="${app.path}" class="${prefix}-card-link">${cardLinkText}</a></div>`;
     return (card);
+  }
 }
   
-  export async function decorateResourceFeed(config, block) {
+export async function decorateResourceFeed(config, block) {
+
+  await lookupPages([], 'resources');
+  const index = window.pageIndex.resources;
+  const sortBy = 'category';
+  if (sortBy && sortOptions(sortBy)) index.data.sort(sortOptions(sortBy));
+  const resourceLibraryContainer = document.createElement('div');
+  resourceLibraryContainer.className = 'resource-library-container';
+  block.append(resourceLibraryContainer);
+
+  index.data.forEach((resourceCard) => {
+    if (resourceCard.title !== '') {
+      const { category } = resourceCard;
+      resourceCard.groupCategory = category;
+    }
+  });
+
+  const groupBy = (array, key) => {
+    return array.reduce((result, currentValue) => {
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      return result;
+    }, {});
+  };
+
+  const resourcesGroupedByCategories = groupBy(index.data, 'groupCategory');
   
-    await lookupPages([], 'resources');
-    const index = window.pageIndex.resources;
-    const sortBy = 'category';
-    if (sortBy && sortOptions(sortBy)) index.data.sort(sortOptions(sortBy));
-    const resourceLibraryContainer = document.createElement('div');
-    resourceLibraryContainer.className = 'resource-library-container';
-    block.append(resourceLibraryContainer);
+  const resourceGroups = Object.entries(resourcesGroupedByCategories);
 
-    index.data.forEach((resourceCard) => {
-      if (resourceCard.title !== '') {
-        const category = resourceCard.category;
-        resourceCard.groupCategory = category;
-      }
-    });
+  resourceGroups.forEach((resourceGroup) => {
+    if (resourceGroup[0] !== 'undefined') {
+      const resourceGroupContainer = document.createElement('div');
+      resourceGroupContainer.className = 'resource-group-container';
+      resourceLibraryContainer.append(resourceGroupContainer);
+      const resourceGroupCategory = document.createElement('div');
+      resourceGroupCategory.className = 'resource-group-title typ-title1';
+      resourceGroupContainer.append(resourceGroupCategory);
+      const resourceGroupTitle = resourceGroup[0].charAt(0).toUpperCase() + resourceGroup[0].slice(1);;
+      resourceGroupCategory.textContent = resourceGroupTitle;
+      resourceGroupCategory.id = resourceGroup[0];
 
-    const groupBy = (array, key) => {
-      return array.reduce((result, currentValue) => {
-        (result[currentValue[key]] = result[currentValue[key]] || []).push(
-          currentValue
-        );
-        return result;
-      }, {});
-    };
+      resourceGroup[1].forEach((resourceGroupItem) => {
+          const cardContainer = document.createElement('ul');
+          cardContainer.append(createResourceCard(resourceGroupItem, 'resource'));
+          resourceGroupContainer.append(cardContainer);
+      });
+    }
+  });
+}
 
-    const resourcesGroupedByCategories = groupBy(index.data, 'groupCategory');
-    
-    const resourceGroups = Object.entries(resourcesGroupedByCategories);
-
-    resourceGroups.forEach((resourceGroup) => {
-      if (resourceGroup[0] !== 'undefined') {
-        const resourceGroupContainer = document.createElement('div');
-        resourceGroupContainer.className = 'resource-group-container';
-        resourceLibraryContainer.append(resourceGroupContainer);
-        const resourceGroupCategory = document.createElement('div');
-        resourceGroupCategory.className = 'resource-group-title typ-title1';
-        resourceGroupContainer.append(resourceGroupCategory);
-        const resourceGroupTitle = resourceGroup[0].charAt(0).toUpperCase() + resourceGroup[0].slice(1);;
-        resourceGroupCategory.textContent = resourceGroupTitle;
-        resourceGroupCategory.id = resourceGroup[0];
-
-        resourceGroup[1].forEach((resourceGroupItem) => {
-            const cardContainer = document.createElement('ul');
-            if (resourceGroupItem.image !== '') {
-              cardContainer.append(createResourceCard(resourceGroupItem, 'resource'));
-              resourceGroupContainer.append(cardContainer);
-            }
-        });
-      }
-    });
-  }
-  
-  export default async function decorate(block) {
-    const config = readBlockConfig(block);
-    block.innerHTML = '';
-    await decorateResourceFeed(config, block);
-  }
+export default async function decorate(block) {
+  const config = readBlockConfig(block);
+  block.innerHTML = '';
+  await decorateResourceFeed(config, block);
+}
   
