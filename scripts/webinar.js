72e12d8513d9d4b0b50b715619ce18f72d101d8b
Meng Tian <mtian@bamboohr.com>
2023-02-09 15:22:27 -0700

    mt fix linting

--- scripts/webinar.js
+++ scripts/webinar.js
@@ -54,7 +54,6 @@ export default async function decorateTemplate(main) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const formSubmit = urlParams.get('formSubmit');
-  const category = getMetadata('category');
 
   if (formSubmit && formSubmit === 'success') {
     main.innerHTML = '';
